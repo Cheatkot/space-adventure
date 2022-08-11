5bac60c5f15b87bfe9a8cb0bde0876c7ac4735f0
tplickat <t.plickat@gmail.com>
2022-08-11 23:20:26 +0200

    initial adding waiting queue

--- roseking/waiting_room/consumers.py
+++ roseking/waiting_room/consumers.py
@@ -1,5 +1,4 @@
 import json
-
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
@@ -31,7 +30,6 @@ def remove_from_waiting_room(room_name, user):
 class WaitingRoom(WebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
-        self.text_data = None
         self.room_name = None
         self.room_group_name = None
         self.user = None
@@ -44,44 +42,40 @@ class WaitingRoom(WebsocketConsumer):
         self.user = self.scope['user']
         add_to_waiting_room(self.room_name, self.user.username)
 
+        self.accept()
+
         async_to_sync(self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name))
-        print('connected')
+
+        print('connected to channel: ' + self.room_group_name)
         print(ActiveWaitingRooms[self.room_name])
 
-        async_to_sync(self.accept())
-
-    def receive(self, text_data):
+    def receive(self, text_data=None, bytes_data=None):
         print('begin receive')
-        self.text_data = json.loads(text_data)
-        print(self.text_data['user'])
-        add_to_waiting_room(self.text_data['user'])
-
+        print(text_data)
         users = get_waiting_room(self.room_name)
+        print(users)
 
-        print(str(users))
-
-        async_to_sync(self.channel_layer.group_send(self.room_group_name, {
-            'type': 'user_list',
-            'users': str(users)
-        }))
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'user.list',
+                'message': 'sad',
+                'users': users
+            }
+        )
+        print('send to group: ' + self.room_group_name)
 
     def disconnect(self, close_code):
         remove_from_waiting_room(self.room_name, self.user.username)
-        async_to_sync(self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
-            self.channel_name))
+            self.channel_name)
 
         users = get_waiting_room(self.room_name)
         print(users)
 
-    # async_to_sync(self.channel_layer.group_send(self.self.room_group_name, {
-    #   'type': 'user_list',
-    #     'users': str(users)
-    # }))
-
     def user_list(self, event):
-        users = event['users']
-
-        async_to_sync(self.send(text_data=users))
+        print('Send Message to WebSocket')
+        self.send(text_data=json.dumps(event))
