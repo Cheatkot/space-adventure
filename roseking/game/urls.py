b74227fd98f00b4772d4fdd8a8d7c728845cc8cc
Cheatkot <52136935+Cheatkot@users.noreply.github.com>
2022-08-14 22:57:34 +0200

    game history added

--- roseking/game/urls.py
+++ roseking/game/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('history/', views.game_history, name='game_history'),
     path('demo/', views.game_demo, name='game_demo'),
     path('<str:game_id>/', views.main_game, name='main_game'),
 ]
--- roseking/game/views.py
+++ roseking/game/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import BrezelWar
 
 
 @login_required
@@ -10,3 +11,12 @@ def game_demo(request):
 @login_required
 def main_game(request, game_id):
     return render(request, "game/main-game.html", {"range": range(9), "game_id": game_id})
+
+
+@login_required
+def game_history(request):
+    ls = BrezelWar.objects.filter(player_one_username__exact=request.user.username)
+    if not ls.exists():
+        ls = BrezelWar.objects.filter(player_two_username__exact=request.user.username)
+
+    return render(request, "game/game-history.html", {"ls": ls})
